909a7a34442564fde2d119b665cdb4cbf17b49a0
Marty <martin.lauterbach@student.reutlingen-university.de>
2024-10-07 16:36:06 +0200

    build textboxes and a catcafecanvas in home

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ErrorPage() {
+    return (
+        <div>
+            <h1>Silly Code: 404</h1>
+            <p> Meowsi, Scooby, this Meowser found no Page for you!</p>
+            <p> you reached a dead end, traveller. Please... CONTACT THE DEVS MEOW!</p>
+            <p>
+                <img src="assets/img/404.jpg" alt="404 Error"/>
+            </p>
+        </div>
+    );
+}
+
+export default ErrorPage;
