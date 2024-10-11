b4c57afc383a540120283209526a0ac20d8036a6
Marty <martin.lauterbach@student.reutlingen-university.de>
2024-10-11 11:20:01 +0200

    Added Credits page and button to Footer
    Signed-off-by: Marty <martin.lauterbach@student.reutlingen-university.de>

---
+++ src/pages/Credits.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Credits() {
+    return (
+        <div>
+            <h1>Here we say who drew the cats</h1>
+            <p>It was artists! not AI! wuhu!</p>
+        </div>
+    );
+}
+
+export default Credits
