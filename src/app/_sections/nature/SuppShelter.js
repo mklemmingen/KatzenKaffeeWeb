26c6d37512fef6ed4013ae6af736f61e35697494
numbarias <m.naegele97@gmail.com>
2024-11-30 21:30:41 +0100

    sections update

--- src/app/_sections/nature/SuppShelter.js
+++ src/app/_sections/nature/SuppShelter.js
@@ -1,26 +1,27 @@
 import React from "react";
-
+import '../_styles/suppShelter.css';
+import CatAnimation from "@/app/_components/CatAnimation";
+import BirdAnimation from "@/app/_components/BirdAnimation";
+import Image from 'next/image';
 function SuppShelter() {
 
     return (
-        <div>
-            <h2> Tierheime unterstützen -&gt; Katzen aus der Natur raus</h2>
-            <p>
-                TODO
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-                sed diam nonumy eirmod tempor invidunt ut labore et dolore
-                magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-                et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-                no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-                consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
-                magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
-                ea
-                rebum.
-                Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            </p>
-        </div>
+                <div className="outer-container">
+                <CatAnimation numberOfCats={3}/>
+                <BirdAnimation numberOfBirds={3}/>
+                <div className="full-container-headline">
+                    <Image src='assets/svg/cat-litter-sand-svgrepo-com.svg' alt="Icon" width={50} height={50} />
+                    <h2 className="header">Tierheime unterstützen</h2>
+                </div>
+                    <div className="full-container">
+                    <div className="left-container">
+                        <p>Tierheime tragen maßgeblich zum Tierschutz bei, indem sie herrenlose Katzen aufnehmen, versorgen und an neue Besitzer vermitteln. Es gibt verschiedene Möglichkeiten, Tierheime zu unterstützen, etwa durch Geldspenden, Sachspenden wie Futter oder Spielzeug sowie ehrenamtliches Engagement. Der Deutsche Tierschutzbund hebt hervor, dass Geldspenden besonders wichtig sind, da viele Tierheime vorrangig finanzielle Mittel benötigen, um steigende Kosten für Tierpflege und medizinische Versorgung zu decken. Über Online-Plattformen wie betterplace.org kannst du gezielt lokale Tierheime unterstützen und so einen wertvollen Beitrag leisten.</p>
+                    </div>
+                    <div className="right-container">
+                        <Image src="/assets/img/cat_tierheim.jpg" alt="cat" width={200} height={200} />
+                    </div>
+                </div>
+            </div>
     );
 }
 
